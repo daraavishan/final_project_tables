5fb72cad4eb878b056d55aaacc3ad354f5985a85
Raghu Betina <raghu@firstdraft.com>
2019-12-07 20:31:43 +0000

    Whitelist columns in UserTopArtistImage for ActiveAdmin

--- app/admin/user_top_artist_images.rb
+++ app/admin/user_top_artist_images.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register UserTopArtistImage do
+
+ permit_params :user_id, :image_url
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
