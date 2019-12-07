9850f37780f855b4d72b435de4865be394509795
Raghu Betina <raghu@firstdraft.com>
2019-12-07 20:31:49 +0000

    Only user_top_artist_image user can modify user_top_artist_image

--- app/controllers/user_top_artist_images_controller.rb
+++ app/controllers/user_top_artist_images_controller.rb
@@ -1,4 +1,14 @@
 class UserTopArtistImagesController < ApplicationController
+  before_action :current_user_must_be_user_top_artist_image_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_user_top_artist_image_user
+    user_top_artist_image = UserTopArtistImage.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == user_top_artist_image.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @user_top_artist_images = UserTopArtistImage.all
 
